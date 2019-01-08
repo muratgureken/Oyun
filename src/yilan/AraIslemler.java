c364a9312ee5c78b7072e554befef1c2f8b28da9
MGUREKEN <MGUREKEN@TOSHIBA_MGUREKEN>
2019-01-08 23:09:30 +0200

    08.01.2019 - yilan kodlaniyor

---
+++ src/yilan/AraIslemler.java
@@ -0,0 +1,8 @@
+package yilan;
+
+public interface AraIslemler {
+	public int YilanIlklendir(int ilkDeger, int yilanBoyu);
+	public void RastgeleSayiUret(int oyunBoyu, int yilanBoyu);
+	public void MatrisCiz(String yon);
+	public void YilaniYerlestir(String tus);
+}
